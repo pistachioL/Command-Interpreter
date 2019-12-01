aac4f589b031465c0b57b6833f1de5525c45adf5
pistachiol <785409363@qq.com>
2019-12-01 21:20:44 +0800

    解决删除不为空的目录

--- rmdir.cpp
+++ rmdir.cpp
@@ -3,19 +3,27 @@
 using namespace std;
    char dirname[20];
 
+
+int rmFile(const char *pathname, const struct stat *sbuf, int type, struct FTW *ftwb)
+{
+    if(remove(pathname) < 0)
+        return -1;
+    return 0;
+}
 void rmdir()
 {
     char dirname[20];
     cin >> dirname;
 
-    if(rmdir(dirname) == 0)
-    //if(nftw(dirname,10, FTW_DEPTH|FTW_MOUNT|FTW_PHYS) < 0)
+    //if(rmdir(dirname) == 0)
+    if(nftw(dirname,rmFile,10, FTW_DEPTH|FTW_MOUNT|FTW_PHYS) < 0)
     {
-        cout << "目录" << dirname << "删除成功！"<<endl; //递归
+       cout << "删除目录"<< dirname << "失败" <<endl;  //权限不够或者没有该文件
     }
     else
     {
-        cout << "删除失败，目录必须为空！" <<endl;  //权限不够或者没有该文件
+        cout << "目录" << dirname << "删除成功！"<<endl; //递归
+
     }
 
 }
