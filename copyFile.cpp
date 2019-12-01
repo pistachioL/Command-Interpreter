4e3b994c56c2f6aac04ff909981d12759da08f69
pistachiol <785409363@qq.com>
2019-12-01 16:52:25 +0800

    实现复制文件

--- copyFile.cpp
+++ copyFile.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 void copyFile()
 {
-    char sourceFile[50],destinationFile[50];  //源文件，副本文件名或路径
-    cin >> sourceFile >> destinationFile;
-    if(access(dirname,F_OK) == -1)  //若文件不存在
+    char srcFile[50],desFile[50];  //源文件，副本文件名或路径
+    FILE *psrc = NULL,*pdes = NULL;
+    char buffer[1024];
+    cin >> srcFile >> desFile;
+    if(access(srcFile,F_OK) == -1)  //若文件不存在
     {
         cout << strerror(2) << endl;
     }
     else
     {
+        psrc = fopen(srcFile,"r");
+        pdes = fopen(desFile,"w+");
+        if(psrc && pdes)
+        {
+            int len = 0;
+            while((len = fread(buffer,1,sizeof(buffer),psrc))>0)  //把源文件的内容读入缓冲区buffer
+            {
+                fwrite(buffer,1,len,pdes);  //把缓冲区buffer的内容写入副本文件
+            }
+            cout << "复制成功！" <<endl;
+        }
+        else
+        {
+            cout << "文件读取失败" <<endl;
+        }
 
+        fclose(psrc);
+        fclose(pdes);
     }
 }
