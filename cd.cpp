70f4fd94d9f6fb721abff164183ef9f4a5e2d050
pistachiol <785409363@qq.com>
2019-12-03 20:57:48 +0800

    能同时创建多个子目录

--- cd.cpp
+++ cd.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cerrno>
 #include <cstring>
+#include <unistd.h>
 using namespace std;
 
 
--- mkdir.cpp
+++ mkdir.cpp
@@ -10,15 +10,42 @@ void mkdir()
 {
     char dirname[20];
     cin >> dirname;
-
-    if(access(dirname,F_OK) == 0)  //判断目录名是否存在，存在返回0，不存在返回-1
+    int len = strlen(dirname);
+    int flag;
+    for(int i=0;i<len;i++)
     {
-        cout << "目录" << dirname << "已存在，创建失败！" <<endl;
+        if(dirname[i] == '/')
+        {
+            dirname[i] = '\0';
+            if(access(dirname,F_OK) != 0)
+            {
+                mkdir(dirname,0777);
+                flag = 1;
+            }
+            else
+                flag = 0;
+
+            dirname[i] = '/';
+
+        }
+    }
+    if(len > 0 && access(dirname,F_OK) != 0)  //判断目录名是否存在，存在返回0，不存在返回-1
+    {
+        mkdir(dirname,0777);
+        flag = 1;
+
+
     }
     else if(mkdir(dirname,0777) == 0)  //mkdir只能创建空的目录包括mkdir -p /test/1/2 创建多个目录
     {
-        cout << "创建成功！" <<endl;
+        flag = 0;
+
     }
+    if(flag == 1)
+        cout << "创建成功！" <<endl;
+    else
+        cout << "目录" << dirname << "已存在，创建失败！" <<endl;
+
 
 
 }
